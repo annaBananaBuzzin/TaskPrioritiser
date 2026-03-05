757ef38932cec467456f0bfae0a95a1c44de1791
Anna-Marie Edeleanu <annabanana@Anna-Maries-MacBook-Air.local>
2026-03-05 15:14:44 +1300

    Project setup and task service

---
+++ src/main/java/com/example/taskprioritiser/TaskPrioritiserApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskprioritiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPrioritiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPrioritiserApplication.class, args);
+	}
+
+}
